eb39ed9ee303ef72e75b8af9d1c7536771b2aa24
scjzhong <scjzhong@sina.com>
2018-12-12 11:43:43 +0800

    测试 server

--- app/server/controller/Chatroom.php
+++ app/server/controller/Chatroom.php
@@ -1,19 +1,63 @@
 <?php
 namespace app\server\controller;
 
+use app\service\RedisService;
+use think\Db;
+use Swoole\WebSocket\Server;
+
 class Chatroom
 {
-    protected $port = 9501;
-    
+    private $host = '0.0.0.0'; 
+    #端口
+    private $port = 9501;
+    #swoole 对象
     private $serv;
     
     public function __construct()
-    {
-        echo '__construct';    
+    {   
+        $this->serv = new Server($this->host, $this->port);
+        $this->serv->set([
+            'worker_num' => 4,
+            'max_request' => 1000,
+            'log_file' => '/var/log/swoole/swoole.log',
+            //'daemonize' => 1,//开启守护进程模式后(daemonize => true)，标准输出将会被重定向到log_file
+        ]);
+        
+        $this->serv->on('satrt', [$this, 'onStart']);
+        $this->serv->on('open', [$this, 'onOpen']);
+        $this->serv->on('receive', [$this, 'onReceive']);
+        $this->serv->on('close', [$this, 'onClose']);
     }
     
-    public function index()
+    /**
+     * 创建指定 worker_num 的work进程
+     */
+    public function onStart($server)
     {
-        echo 'Chatroom';
+        var_dump($server);
+        #初始化 数据库 redis 长连接
+        Db::connect();
+        RedisService::getInstance();
     }
+    
+    public function onOpen(\swoole_websocket_server $server, \swoole_http_request $request)
+    {
+        
+        echo 'onOpen：' . $request->fd;
+        echo PHP_EOL;
+    }
+    
+    
+    public function onReceive(\swoole_websocket_server $server, \Swoole\WebSocket\Frame $frame)
+    {
+        echo 'onReceive：' . $frame->data;
+        echo PHP_EOL;
+    }
+    
+    public function onClose(\swoole_websocket_server $server, $fd)
+    {
+        echo 'onClose：' . $fd;
+        echo PHP_EOL;
+    }
+    
 }
--- app/service/RedisService.php
+++ app/service/RedisService.php
@@ -14,9 +14,9 @@ class RedisService
     public $redis;
     
     protected $options = [
-        'host'       => '127.0.0.1',
+        'host'       => '47.100.161.112',
         'port'       => 6380,
-        'password'   => '123456',
+        'password'   => 'redis_nihao123###',
         'select'     => 14,
         'timeout'    => 0,
         'expire'     => 0,
