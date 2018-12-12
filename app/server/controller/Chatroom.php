a0df29f6e0cd71d34ba2a6b9d37f1b603047f9bf
scjzhong <scjzhong@sina.com>
2018-12-12 09:59:16 +0800

    init server 控制器

--- app/server/controller/Chatroom.php
+++ app/server/controller/Chatroom.php
@@ -3,6 +3,15 @@ namespace app\server\controller;
 
 class Chatroom
 {
+    protected $port = 9501;
+    
+    private $serv;
+    
+    public function __construct()
+    {
+        echo '__construct';    
+    }
+    
     public function index()
     {
         echo 'Chatroom';
