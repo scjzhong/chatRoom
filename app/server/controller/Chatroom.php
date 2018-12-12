0f61103d1257ea4ce0c859a59f4d124b722a00c7
scjzhong <scjzhong@sina.com>
2018-12-12 09:49:04 +0800

    新建server 模块入口文件

---
+++ app/server/controller/Chatroom.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\server\controller;
+
+class Chatroom
+{
+    public function index()
+    {
+        echo 'Chatroom';
+    }
+}
--- public/server.php
+++ public/server.php
@@ -3,5 +3,12 @@
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../app/');
+
+// 绑定模块
+define('BIND_MODULE', 'server/Chatroom');
+
+// 定义 root 路径
+define('ROOT_PATH', __DIR__ . '/../');
+
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
